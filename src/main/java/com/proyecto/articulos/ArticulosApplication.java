a797930572873da01ce527d23c7775bfc39f09de
Juan Core i5 <mail@falso.com>
2026-07-02 03:08:49 -0300

    Agregado un temporizador para cerrar el programa cuano no se usa, clase Timer

--- src/main/java/com/proyecto/articulos/ArticulosApplication.java
+++ src/main/java/com/proyecto/articulos/ArticulosApplication.java
@@ -3,10 +3,22 @@ package com.proyecto.articulos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.proyecto.articulos.util.Timer;
+
+
 @SpringBootApplication
 public class ArticulosApplication {
 
 	public static void main(String[] args) {
+        //Agrego un Timer porque siempre me olvido de apagar el programa cuando me voy de casa.
+        int segundos = 3600;
+        boolean apagadoAutomatico = true;
+        if(apagadoAutomatico){
+        System.out.println("Se tendría que apagar en: "+segundos+" segundos");
+        Timer apagadoEn = new Timer(segundos, "Se apagó en esta cantidad se sgundos; ");
+        apagadoEn.setTimer();
+        }
+        //System.out.println("después de if");
 		SpringApplication.run(ArticulosApplication.class, args);
 	}
 
---
+++ src/main/java/com/proyecto/articulos/util/Timer.java
@@ -0,0 +1,40 @@
+package com.proyecto.articulos.util;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.time.LocalTime;
+
+public class Timer {
+            // 1. Create a scheduler thread pool
+        private ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        private int segundos;
+        private String mensaje;
+        private LocalTime horaActual = LocalTime.now();
+        //private SpringApplication aplicacion;
+
+        //Le agrego constructor vacío porque se usa dentro de springboot, por las dudas
+        public Timer(){};
+        public Timer(int segundos, String mensaje){
+            this.segundos  = segundos;
+            this.mensaje = mensaje;
+            //this.aplicacion = aplicacion;
+        };
+
+        public void setTimer(){
+
+        // 2. Schedule a task (equivalent to setTimeout)
+        scheduler.schedule(() -> {
+            System.out.println(mensaje + segundos);
+            System.out.println("Hora de cierre de la aplicación: " + horaActual);
+            System.exit(1);
+            //Me falta encontrar la manera que acceda a la aplicación para cerrarla
+            //SpringBootApplication.close();
+        }, segundos, TimeUnit.SECONDS);
+        
+        // Remember to shut down the scheduler when your app closes so the JVM can exit
+        scheduler.shutdown();
+        }
+
+    }
+
